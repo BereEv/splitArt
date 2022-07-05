d99deadfc139bb4e6e13935cbd12eb4d5542b3e7
BereEv <99802438+BereEv@users.noreply.github.com>
2022-07-05 12:59:14 +0300

    Update Splir.py

--- Splir.py
+++ Splir.py
@@ -3,16 +3,12 @@ import glob
 
 
 def split_art():
-    f = glob.glob('files/*.xlsx')
-    files = f
-    df = [pd.read_excel(f, header=0) for f in files]
+    df = [pd.read_excel(f, header=0) for f in glob.glob('files/*.xlsx')]
     art = pd.concat(df, keys=glob.glob('files/*xlsx'))
     names_cols = list(art.columns)
     art = art.rename(columns={names_cols[0]: 'Артикул'})
     art[['Art1', 'Art2', 'Art3']] = art['Артикул'].str.split('-', n=2, expand=True)
-    art.info()
     art.to_excel('log/new.xlsx')
-    print('Готово')
 
 
 split_art()
